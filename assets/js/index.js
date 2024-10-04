82a358f749d6aeaa30a0e4e922f80ab2282c17da
MarinaArrieta <arrietamarina12@gmail.com>
2024-10-04 16:40:39 -0300

    Agregado de Footer, estilos para las cards, cambios en funcion de mostrar Imagenes en las cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,9 @@ async function getApiMarvel(){
         const response = await fetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=20ef376510097f50f89a7cf2b98cc1ce&hash=3c0bdb616f415c7a9a47908b7f5d4814');
         const parsedMarvel = await response.json();
         console.log(parsedMarvel);
+        
+        let totalComics = parsedMarvel.data.total;
+        display_total(totalComics)
         styleCardComics(parsedMarvel.data.results);
     }
     catch(error){
@@ -13,34 +16,31 @@ async function getApiMarvel(){
 getApiMarvel();
 
 const tipoComicCharacter = document.getElementById('tipo-comic-character');
+function display_total(total){
+    let comicsResults = document.getElementById('comics-results');
+    comicsResults.innerText = total + ' RESULTADOS';
+}
+
 const marvelList = document.getElementById('marvel-list');
-
-// tipoComicCharacter.addEventListener('change', ()=> {
-//     filterComicCharacter();
-// });
-
-// function filterComicCharacter(){
-//     if (tipoComicCharacter.value === 'comic'){
-//         styleCardComics();
-//     }
-// }
-
-function styleCardComics(characters){
-    console.log(characters)
-    characters.forEach(character => {
+function styleCardComics(comics){
+    console.log(comics)
+    comics.forEach(comic => {
         const list = document.createElement('li');
         const image = document.createElement('img');
         const tittle = document.createElement('h3');
-        if (character.images.length > 0){
-            console.log(character.images)
-            image.src = character.images[0].path + '.' + character.images[0].extension;
-        }
+        // if (comic.images.length > 0){
+        //     console.log(comic.images)
+            image.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
+        // }
         
-        tittle.textContent = character.title;
-
-        image.style.width = '10%';
+        tittle.innerText = comic.title;
+        list.style.width = '90%';
+        list.style.display = 'flex';
+        list.style.flexDirection = 'column';
+        list.style.gap = '10px';
+        image.style.width = '100%';
         tittle.style.fontSize = '1.2rem';
-        tittle.style.color = 'blue';
+        tittle.style.color = '#607d8b';
 
         list.appendChild(image);
         list.appendChild(tittle);
