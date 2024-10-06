428d36ddac23e29d1e70ffaf9c5b439cd158d33b
MarinaArrieta <arrietamarina12@gmail.com>
2024-10-05 22:17:55 -0300

    Funcion de ordenar por titulo de AZ o ZA, acomodo de cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,49 @@
 const url = 'http://gateway.marvel.com/';
 const urlComics = 'v1/public/comics';
 const urlCharacters = 'v1/public/characters';
-const keyHash = '?ts=1&apikey=20ef376510097f50f89a7cf2b98cc1ce&hash=3c0bdb616f415c7a9a47908b7f5d4814';
+const keyHash = 'ts=1&apikey=20ef376510097f50f89a7cf2b98cc1ce&hash=3c0bdb616f415c7a9a47908b7f5d4814';
 const modeLightDark = document.getElementById('mode-light-dark');
 const divModeButton = document.getElementById('div-mode-button');
 let mode = 'dark';
 const iconMode = document.getElementById('icon-mode');
-const tipoComicCharacter = document.getElementById('tipo-comic-character');
-const searchInput = document.getElementById('search-input');
+let tipoComicCharacter = document.getElementById('tipo-comic-character');
+let searchInput = document.getElementById('search-input');
 const marvelList = document.getElementById('marvel-list');
+let order = document.getElementById('order');
 
 async function getApiMarvel(){
     try{
-        let inputType = tipoComicCharacter.value
-        let inputSearch = searchInput.value
+        let inputType = tipoComicCharacter.value;
+        let inputSearch = searchInput.value;
+        let orderBy = order.value;
+
         console.log('input searh',inputType)
+       
         if (inputType == 'comic'){
-            search_param = '&titleStartsWith='
-            url_type = urlComics
+            search_param = '&titleStartsWith=';
+            urlType = urlComics;
         } else {
-            search_param = '&nameStartsWith='
-            url_type = urlCharacters
+            search_param = '&nameStartsWith=';
+            urlType = urlCharacters;
         }
 
-        let filter_title ='';
+        let filterTitle = '';
         if (inputSearch){
-            filter_title = search_param + inputSearch;   
+            filterTitle = search_param + inputSearch;   
+        }
+        
+        if(orderBy == 'az' && inputType == 'comic'){
+            orderParam = '?orderBy=title&';
+        } 
+        else if (orderBy == 'za' && inputType == 'comic'){
+            orderParam = '?orderBy=-title&';
         }
 
-        url_final = url+url_type+keyHash + filter_title;
+        url_final = url + urlType + orderParam + filterTitle + keyHash ;
 
         marvelList.innerHTML = '';
         const response = await fetch(url_final);
+        console.log('hola',url_final)
         console.log(response)
         const parsedMarvel = await response.json();
         console.log(parsedMarvel);
@@ -51,7 +63,6 @@ document.getElementById("form-search").addEventListener("submit", function(event
 function modeLD(){
     if (mode === 'dark'){
         document.body.style.background = '#181818';
-        // divModeButton.style.background = '#181818';
         iconMode.className = "fa-solid fa-sun";
         iconMode.style.fontSize = '1.5rem';
         iconMode.style.textAlign = 'center';
@@ -64,7 +75,6 @@ function modeLD(){
     } 
     else{
         document.body.style.background = '#e2e8f0';
-        // divModeButton.style.background = '#e2e8f0';
         iconMode.className = "fa-solid fa-moon";
         iconMode.style.fontSize = '1.5rem';
         iconMode.style.textAlign = 'center';
@@ -96,17 +106,17 @@ function styleCardComics(comics){
         const list = document.createElement('li');
         const image = document.createElement('img');
         const tittle = document.createElement('h3');
-        // if (comic.images.length > 0){
-        //     console.log(comic.images)
-            image.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
-        // }
+
+        image.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
         
         tittle.innerText = comic.title;
-        list.style.width = '90%';
+        list.style.width = '200px';
+        list.style.height = '400px';
         list.style.display = 'flex';
         list.style.flexDirection = 'column';
         list.style.gap = '10px';
         image.style.width = '100%';
+        image.style.height = '300px';
         tittle.style.fontSize = '1.2rem';
         tittle.style.color = '#607d8b';
         list.appendChild(image);
@@ -121,17 +131,18 @@ function styleCardCharacters(comics){
         const list = document.createElement('li');
         const image = document.createElement('img');
         const name = document.createElement('h3');
-        // if (comic.images.length > 0){
-        //     console.log(comic.images)
-            image.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
-        // }
+
+        image.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
         
         name.innerText = comic.name;
         list.style.width = '90%';
         list.style.display = 'flex';
         list.style.flexDirection = 'column';
         list.style.gap = '10px';
+        console.log(image)
         image.style.width = '100%';
+        // image.style.height = '300px';
+
         name.style.fontSize = '1.2rem';
         name.style.color = '#607d8b';
         list.appendChild(image);
